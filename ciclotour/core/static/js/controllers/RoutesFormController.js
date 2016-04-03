81a16ee9c6d32b1ae6e0aa23692938d9710210bb
Guilherme <guilherme_hubner@msn.com>
2016-04-03 13:44:16 -0300

    Trace route feature

--- ciclotour/core/static/js/controllers/RoutesFormController.js
+++ ciclotour/core/static/js/controllers/RoutesFormController.js
@@ -1,95 +1,115 @@
 angular.module("ciclotourApp").controller('RoutesFormController', function($scope, $http){
     $scope.origin = "";
-    $scope.destination = "";
-
-    $scope.originCoordinates = null;
-    $scope.destinationCoordinates = null;
+    $scope.wayPoints = [];
 
     $scope.setOrigin = function (){
+
+        $scope.wayPoints = [];
+
+        if($scope.origin != "") {
             var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $scope.origin.toString();
-            if($scope.origin != "") {
-                $http.get(url).success(function(data){
-                    $scope.originCoordinates = {
-                        lat: data.results[0].geometry.location.lat,
-                        lng: data.results[0].geometry.location.lng
-                    };
+            $http.get(url).success(function(data){
+                var originCoordinates = {
+                    lat: data.results[0].geometry.location.lat,
+                    lng: data.results[0].geometry.location.lng
+                };
 
-                    initMap($scope.originCoordinates, $scope.destinationCoordinates, "map");
-                });
-            }
-            else {
-                $scope.originCoordinates = null;
-                initMap($scope.originCoordinates, $scope.destinationCoordinates, "map");
-            }
-        };
-
-    $scope.setDestination = function (){
-            if($scope.destination) {
-                var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $scope.destination.toString();
-                $http.get(url).success(function(data){
-                    $scope.destinationCoordinates = {
-                        lat: data.results[0].geometry.location.lat,
-                        lng: data.results[0].geometry.location.lng
-                    };
-
-                    initMap($scope.originCoordinates, $scope.destinationCoordinates, "map");
-                });
-            }
-            else {
-                $scope.destinationCoordinates = null;
-                initMap($scope.originCoordinates, $scope.destinationCoordinates, "map");
-            }
-        };
-
-        function startMap(center, target){
-            return map = new google.maps.Map(document.getElementById(target), {
-                center: center,
-                scrollwheel: true,
-                zoom: 10
+                initMap(originCoordinates, "map");
             });
         }
+        else {
+            var originCoordinates = null;
+            initMap(originCoordinates, "map");
+        }
+    };
 
-        function addMarker(map, coordinates, title){
-            return new google.maps.Marker({
-                map: map,
-                position: coordinates,
-                title: title
-            });
+    $scope.undo = function(){
+        if($scope.wayPoints.length <= 1)
+        {
+            $scope.setOrigin();
+            $scope.wayPoints = [];
+            return;
         }
 
-        function setZoom(map, originMarker, destinationMarker){
-            var bounds = new google.maps.LatLngBounds();
-            bounds.extend(originMarker.position);
-            bounds.extend(destinationMarker.position);
+        $scope.wayPoints.pop();
+
+        var map = startMap($scope.wayPoints[0], "map");
+        var oldList = $scope.wayPoints.slice(0);
+        $scope.wayPoints = [];
+
+        while(oldList.length > 0)
+            addWayPoint(oldList.shift(), map);
+
+        var bounds = new google.maps.LatLngBounds();
+            bounds.extend($scope.wayPoints[0]);
+            bounds.extend($scope.wayPoints[$scope.wayPoints.length-1]);
             map.fitBounds(bounds);
+    };
+
+    function startMap(center, target){
+        var mapOptions = {
+            center: center,
+            scrollwheel: false,
+            streetViewControl: false,
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.HYBRID //ROADMAP, SATELLITE, HYBRID, TERRAIN
+        };
+
+        var mapDiv = document.getElementById(target);
+
+        var map = new google.maps.Map(mapDiv, mapOptions);
+
+        google.maps.event.addListener(map, "click", function(event) {
+            addWayPoint(event.latLng, map);
+        });
+
+        return map;
+    }
+
+    function addWayPoint(coordinates, map){
+        new google.maps.Marker({
+            position: coordinates,
+            map: map,
+            icon: "http://" + window.location.host+"/static/img/mapIcons/marker/black" +
+            ($scope.wayPoints.length + 1) + ".png"
+        });
+
+        if($scope.wayPoints.length > 0)
+            renderRoute($scope.wayPoints[$scope.wayPoints.length-1], coordinates, map);
+
+        $scope.wayPoints.push(coordinates);
+    }
+
+    function renderRoute(originCoordinates, destinationCoordinates, map){
+        var renderer = new google.maps.DirectionsRenderer({
+	            'draggable': false,
+	            'suppressMarkers': true,
+	            'preserveViewport': true
+	        });
+	        renderer.setMap(map);
+	        renderer.setPanel(document.getElementById("directionsPanel"));
+	        var directionsService = new google.maps.DirectionsService();
+
+	        directionsService.route({
+	                origin: originCoordinates,
+	                destination: destinationCoordinates,
+	                travelMode: google.maps.TravelMode.BICYCLING
+	            }, function(response, status) {
+	                if (status === google.maps.DirectionsStatus.OK) {
+	                    renderer.setDirections(response);
+	                } else {
+	                    console.log('Directions request failed due to ' + status);
+	                }
+            });
+    }
+
+    function initMap(origin, target) {
+        if(origin != null)            {
+            map = startMap(origin, target);
         }
-
-        function initMap(origin, destination, target) {
-            if(origin != null && destination != null){
-                map = startMap(origin, target);
-
-                // Create markers and set its position.
-                originMarker = addMarker(map, origin, "Origem!");
-                destinationMarker = addMarker(map, destination, "Destino!");
-
-                //Fit map
-                setZoom(map, originMarker, destinationMarker);
-            }
-            else if(origin != null)            {
-                map = startMap(origin, target);
-
-                // Create a marker and set its position.
-                originMarker = addMarker(map, origin, "Origem!");
-            }
-            else if(destination != null) {
-                map = startMap(destination, target);
-
-                // Create a marker and set its position.
-                originMarker = addMarker(map, destination, "Destino!");
-            }
-            else{
-                $(target).html("");
-                $(target).removeAttr('style');
-            }
+        else{
+            $(target).html("");
+            $(target).removeAttr('style');
         }
+    }
 });
