e3a02a6dcd05b3bc9c6626449f4708198505ac4c
Guilherme <guilherme_hubner@msn.com>
2016-04-10 00:23:04 -0300

    Manual route option

--- ciclotour/core/static/js/controllers/RoutesFormController.js
+++ ciclotour/core/static/js/controllers/RoutesFormController.js
@@ -1,19 +1,34 @@
 angular.module("ciclotourApp").controller('RoutesFormController', function($scope, $http){
-    $scope.origin = "";
-    $scope.wayPoints = [];
+    $scope.origin = ""; //origin name get from user entry
+    $scope.wayPoints = []; //the list of points will be sent to server
+    $scope.mapMarkers = []; //the list of route markers
+    $scope.mapPolylines = []; //the list of routes path's
+    $scope.manualMode = false; //boolean which indicates if path should be drawn by google or
+    // simple straight line
 
+    /******************************************
+     * This method try to find one place through
+     * user entry and init one map centralized in
+     * this place
+    *******************************************/
     $scope.setOrigin = function (){
-
         $scope.wayPoints = [];
+        $scope.mapMarkers = [];
+        $scope.mapPolylines = [];
 
         if($scope.origin != "") {
-            var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $scope.origin.toString();
+            //Build the URL to get place from google maps api
+            var url = "https://maps.googleapis.com/maps/api/geocode/json?address="
+                + $scope.origin.toString();
+
+            //Performs request and get coordinates
             $http.get(url).success(function(data){
                 var originCoordinates = {
                     lat: data.results[0].geometry.location.lat,
                     lng: data.results[0].geometry.location.lng
                 };
 
+                //Init map
                 initMap(originCoordinates, "map");
             });
         }
@@ -23,42 +38,43 @@ angular.module("ciclotourApp").controller('RoutesFormController', function($scop
         }
     };
 
+    /******************************************
+     * Undo the last change done by user
+    *******************************************/
     $scope.undo = function(){
-        if($scope.wayPoints.length <= 1)
-        {
-            $scope.setOrigin();
-            $scope.wayPoints = [];
+        if($scope.wayPoints.length == 0)
             return;
-        }
 
         $scope.wayPoints.pop();
+        $scope.mapMarkers.pop().setMap(null);
 
-        var map = startMap($scope.wayPoints[0], "map");
-        var oldList = $scope.wayPoints.slice(0);
-        $scope.wayPoints = [];
-
-        while(oldList.length > 0)
-            addWayPoint(oldList.shift(), map);
-
-        var bounds = new google.maps.LatLngBounds();
-            bounds.extend($scope.wayPoints[0]);
-            bounds.extend($scope.wayPoints[$scope.wayPoints.length-1]);
-            map.fitBounds(bounds);
+        if($scope.mapPolylines.length > 0)
+            $scope.mapPolylines.pop().setMap(null);
     };
 
+    /******************************************
+     * Responsible method to render map
+    *******************************************/
     function startMap(center, target){
+        //Set map configurations
         var mapOptions = {
             center: center,
             scrollwheel: false,
             streetViewControl: false,
             zoom: 12,
+            draggableCursor: 'default',
             mapTypeId: google.maps.MapTypeId.HYBRID //ROADMAP, SATELLITE, HYBRID, TERRAIN
         };
 
+        //Get map container element
         var mapDiv = document.getElementById(target);
 
+        //Instanciate the map object
         var map = new google.maps.Map(mapDiv, mapOptions);
 
+        //Add click event listener.
+        //For each user click on map, get the coordinates, create a marker and
+        //trace route between last marker and actual position requested by user
         google.maps.event.addListener(map, "click", function(event) {
             addWayPoint(event.latLng, map);
         });
@@ -66,45 +82,146 @@ angular.module("ciclotourApp").controller('RoutesFormController', function($scop
         return map;
     }
 
+    /******************************************
+     * Responsible method to create a marker and
+     * trace route between last marker and actual position
+     * requested by user
+    *******************************************/
     function addWayPoint(coordinates, map){
-        new google.maps.Marker({
+        if($scope.wayPoints.length > 0) { //Case this is the second way point or above
+
+            if($scope.manualMode){ //If manual, mode must render a linear route
+                //Defines the path as the last way point added until the actual informed
+                var path = [$scope.wayPoints[$scope.wayPoints.length - 1].coordinates, coordinates];
+
+                //Render the straight line
+                renderLinearRoute(path, map);
+
+                //Add marker on map
+                addMarker(coordinates, map);
+
+                //Add actual way point
+                $scope.wayPoints.push({
+                    type: 'L', // LINEAR
+                    coordinates: coordinates
+                });
+            }
+            else{ //If not manual mode, must render a route indicated by google
+                renderRoute($scope.wayPoints[$scope.wayPoints.length - 1].coordinates,
+                    coordinates, map);
+            }
+        }
+        else{ // Case this is the first way point, only add a marker on map
+            addMarker(coordinates, map);
+
+            //Add actual way point
+            $scope.wayPoints.push({
+                type: 'N', //NONE
+                coordinates: coordinates
+            });
+        }
+    }
+
+    /******************************************
+     * Responsible method to render a marker on
+     * map and add it to marker's list
+    *******************************************/
+    function addMarker(coordinates, map){
+        //Create marker and render it on map
+        var marker = new google.maps.Marker({
             position: coordinates,
             map: map,
             icon: "http://" + window.location.host+"/static/img/mapIcons/marker/black" +
             ($scope.wayPoints.length + 1) + ".png"
         });
 
-        if($scope.wayPoints.length > 0)
-            renderRoute($scope.wayPoints[$scope.wayPoints.length-1], coordinates, map);
-
-        $scope.wayPoints.push(coordinates);
+        //Add marker on list
+        $scope.mapMarkers.push(marker);
     }
 
-    function renderRoute(originCoordinates, destinationCoordinates, map){
-        var renderer = new google.maps.DirectionsRenderer({
-	            'draggable': false,
-	            'suppressMarkers': true,
-	            'preserveViewport': true
-	        });
-	        renderer.setMap(map);
-	        renderer.setPanel(document.getElementById("directionsPanel"));
-	        var directionsService = new google.maps.DirectionsService();
+    /******************************************
+     * Responsible method to render a polyline(path)
+     * on map and add it to polyline's list
+    *******************************************/
+    function renderLinearRoute(path, map){
+        //Create polyline and render it on map
+        var polyLine = new google.maps.Polyline({
+            path: path,
+            map: map,
+            geodesic: true,
+            strokeColor: '#000000',
+            strokeWeight: 6,
+            strokeOpacity: 0.6
+        });
 
-	        directionsService.route({
-	                origin: originCoordinates,
-	                destination: destinationCoordinates,
-	                travelMode: google.maps.TravelMode.BICYCLING
-	            }, function(response, status) {
-	                if (status === google.maps.DirectionsStatus.OK) {
-	                    renderer.setDirections(response);
-	                } else {
-	                    console.log('Directions request failed due to ' + status);
-	                }
+        //Add polyline on list
+        $scope.mapPolylines.push(polyLine);
+    }
+
+    /******************************************
+     * Responsible method to request a route from
+     * google and render it on map
+    *******************************************/
+    function renderRoute(originCoordinates, destinationCoordinates, map){
+        //Starts the google directions service
+        var directionsService = new google.maps.DirectionsService();
+
+        //Performs the bicycling route request between the origin and destination
+        directionsService.route({
+                origin: originCoordinates,
+                destination: destinationCoordinates,
+                travelMode: google.maps.TravelMode.BICYCLING //BICYCLING, DRIVING, TRANSIT, WALKING
+            },
+            function(response, status) {
+                //If success on request, render the route
+                if (status === google.maps.DirectionsStatus.OK) {
+                    var path = [];
+                    var legs = response.routes[0].legs;
+
+                    var myroute = response.routes[0].legs[0];
+
+                    var originCoordinates = myroute.steps[0].start_point;
+                    var destinationCoordinates = myroute.steps[myroute.steps.length - 1].end_point;
+
+                    //Check if google have changed the origin coordinates, case true,
+                    //add a path to link the last waypoint to new google route
+                    if($scope.wayPoints[$scope.wayPoints.length - 1].coordinates != originCoordinates)
+                        path.push($scope.wayPoints[$scope.wayPoints.length - 1].coordinates);
+
+                    //Add marker on list
+                    addMarker(destinationCoordinates, map);
+
+                    //Add waypoint on list
+                    $scope.wayPoints.push({
+                        type: 'G', //GOOGLE
+                        coordinates: destinationCoordinates
+                    });
+
+                    //Build the path to render a polyline
+                    for (i = 0; i < legs.length; i++) {
+                        var steps = legs[i].steps;
+                        for (j = 0; j < steps.length; j++) {
+                            var nextSegment = steps[j].path;
+                            for (k = 0; k < nextSegment.length; k++) {
+                                path.push(nextSegment[k]);
+                            }
+                        }
+                    }
+
+                    //Render the polyline with the google path on map
+                    renderLinearRoute(path, map);
+                }
+                else { // If request failed, log it on console
+                    console.log('Directions request failed due to ' + status);
+                }
             });
     }
 
+    /******************************************
+     * Responsible method to initiate the map
+    *******************************************/
     function initMap(origin, target) {
-        if(origin != null)            {
+        if(origin != null){
             map = startMap(origin, target);
         }
         else{
